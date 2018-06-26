32fb56ad74f9e42f745a3dbf7e1dc2b33e9385be
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-06-26 22:00:33 +0900

    first commit

--- demo_main.php
+++ demo_main.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="js/jquery-2.1.3.min.js"></script>
     <script src="js/demo_main.js"></script>
-    <link rel="shortcut icon" href="img/post.jpg">
+    <!-- <link rel="shortcut icon" href="img/post.jpg"> -->
     <link rel="stylesheet" href="css/style.css">
     <title>POST</title>
 </head>
@@ -36,13 +36,94 @@
 
     <!-- コンテンツ表示画面 -->
     <div id="main">
-        <div id="loginmenu">
-            <div><input id="email" type="text" placeholder="メールアドレス"></div>
-            <div><input id="pass" type="password" placeholder="パスワード"></div>
-            <div><button id="login">ログイン</button></div>
-            <div><button id="makeaccount">アカウント作成</button></div>
-        </div>
-        <div id="imglist">
+        <div id='group' style="width: 100%;display:flex;">
+            <div id="todo" class='task_grp'>
+                <div class='task_grp_header' style='display:flex;'>
+                    <p class='task_grp_title' style='background-color:#f2af1d;'>ToDo</p>
+                    <p class='grp_add'>＋</a>
+                    <p class='grp_set'>◎</a>
+                </div>
+                <div id="todo1" class='task' dropzone="move" draggable="true">
+                    <div class='task_title high'>
+                        ログイン
+                    </div>
+                    <div>
+                        メンバー
+                    </div>
+                    <div>
+                        更新日付
+                    </div>
+                </div>
+                <div id="todo2" dropzone="move" class='task' draggable="true">
+                    <div class='task_title high'>
+                        デモページ作成
+                    </div>
+                    <div>
+                        メンバー
+                    </div>
+                    <div>
+                        更新日付
+                    </div>
+                </div>
+            </div>
+            <div id="finish" class='task_grp'>
+                <div class='task_grp_header' style='display:flex;'>
+                    <p class='task_grp_title' style='background:#6091d3;'>Finish</p>
+                    <p class='grp_add'>＋</a>
+                    <p class='grp_set'>◎</a>
+                </div>
+                <div id="finish1" dropzone="move" class='task' draggable="true">
+                    <div class='task_title middle'>
+                        ログイン
+                    </div>
+                    <div>
+                        メンバー
+                    </div>
+                    <div>
+                        更新日付
+                    </div>
+                </div>
+                <div id="finish2" dropzone="move" class='task' draggable="true">
+                    <div class='task_title middle'>
+                        デモページ作成
+                    </div>
+                    <div>
+                        メンバー
+                    </div>
+                    <div>
+                        更新日付
+                    </div>
+                </div>
+            </div>
+            <div id="confirm" class='task_grp'>
+                <div class='task_grp_header' style='display:flex;'>
+                    <p class='task_grp_title' style='background-color:#71ce88;'>Confirm</p>
+                    <p class='grp_add'>＋</a>
+                    <p class='grp_set'>◎</a>
+                </div>
+                <div id="confirm1" dropzone="move" class='task' draggable="true">
+                    <div class='task_title low'>
+                        ログイン
+                    </div>
+                    <div>
+                        メンバー
+                    </div>
+                    <div>
+                        更新日付
+                    </div>
+                </div>
+                <div id="confirm2" dropzone="move" class='task' draggable="true">
+                    <div class='task_title low'>
+                        デモページ作成
+                    </div>
+                    <div>
+                        メンバー
+                    </div>
+                    <div>
+                        更新日付
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
