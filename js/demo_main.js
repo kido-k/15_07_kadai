f23b66f62c53b84090ce94632d2d3c34a848823a
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-07-07 05:07:42 +0900

    Revert "first commit"
    This reverts commit 32fb56ad74f9e42f745a3dbf7e1dc2b33e9385be.

--- js/demo_main.js
+++ js/demo_main.js
@@ -10,8 +10,7 @@ var userimage_url;
 var displayLoginBar = false;
 
 $(function () {
-    // loadDemoAllImage();
-    // loadDemoAllImage();
+    loadDemoAllImage();
     //EnterKeyで送信する
     $("#search").keyup(function (event) {
         event = event || window.event;
@@ -43,7 +42,7 @@ $(function () {
             },
             success: function (res) {
                 // console.log("ajax通信に成功しました");
-                sessionStorage.setItem(['user_id'], [res]);
+                sessionStorage.setItem(['user_id'],[res]);
                 $("#loginmenu").css({ display: 'none' });
                 $("#logout").css({ display: 'inline-block' });
             }
@@ -54,37 +53,7 @@ $(function () {
     $("#makeaccount").on("click", function () {
         window.location.href = "entry.php";
     });
-
-    $('.task').on('dragstart', f_dragstart);
-    $('.task_grp').on('dragover', f_dragover);
-    $('.task_grp').on('drop', f_drop);
 });
-
-/***** ドラッグ開始時の処理 *****/
-function f_dragstart(event) {
-    event = event.originalEvent;
-    //ドラッグするデータのid名をDataTransferオブジェクトにセット
-    event.dataTransfer.setData("text", event.target.id);
-}
-
-/***** ドラッグ要素がドロップ要素に重なっている間の処理 *****/
-function f_dragover(event) {
-    //dragoverイベントをキャンセルして、ドロップ先の要素がドロップを受け付けるようにする
-    event.preventDefault();
-}
-
-/***** ドロップ時の処理 *****/
-function f_drop(event) {
-    event = event.originalEvent;
-    //ドラッグされたデータのid名をDataTransferオブジェクトから取得
-    var id_name = event.dataTransfer.getData("text");
-    //id名からドラッグされた要素を取得
-    var drag_elm = document.getElementById(id_name);
-    //ドロップ先にドラッグされた要素を追加
-    event.currentTarget.appendChild(drag_elm);
-    //エラー回避のため、ドロップ処理の最後にdropイベントをキャンセルしておく
-    event.preventDefault();
-}
 
 
 function controlMenu() {
@@ -102,6 +71,7 @@ function controlMenu() {
 }
 
 function btnAction() {
+
     //homeボタン
     $("#home_button").on("click", function () {
         $("#imglist").css({ display: 'inline-block' });
@@ -110,15 +80,36 @@ function btnAction() {
 };
 
 function loadDemoAllImage() {
-    for (i = 0; i < result.length; i++) {
-        img_id = result[i].img_id;
-        $("#todo").append('<div style="display:flex;"><h2 id="' + 'img_name' + img_id + '" style="height:30px;width:75%;margin:0;margin-left:20px;margin-top: 20px;">' + result[i].img_name + '</h2>'
-            + '<h2 id="' + 'sysdate' + img_id + '" style="height:30px;width:20%;margin:0;text-align:right;margin-top: 20px;">' + result[i].sysdate + '</h2></div>');
-        $("#finish").append('<div style="display:flex;"><h3 id="' + 'account' + img_id + '" style="margin:0;margin-left:20px;">' + 'by ' + result[i].account + '</h3>'
-            + '<p id="' + 'follow' + img_id + '" class="follow" style="font-size:12px;margin:0;margin-left:50px;cursor:pointer;border-radius:50px;background-color:#FFF;border:1px solid #1da1f2;color:#1da1f2;width:90px; text-align:center">FOLLOW ME</p>'
-            + '<p style="font-size:12px;margin:0;margin-left:50px">いいね</p><img id="' + 'good' + img_id + '"src="img/heart_blank.png" class="good" style="height:15px;width:15px;margin:0;margin-left:10px;cursor:pointer;"></div>');
-        $("#confirm").append('<div id="' + 'img' + img_id + '" style="display:flex; height:220px;margin-left:15px;"></div>');
-    };
+    var result = "";
+    $.ajax({
+        url: "demo_select_allimginfo.php",
+        type: "POST",
+        dataType: "text",
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // console.log("ajax通信に失敗しました");
+        },
+        success: function (res) {
+            // console.log("ajax通信に成功しました");
+            // console.log(res);
+            result = JSON.parse(res);
+            // console.log(result[0]);
+            var img_id = "";
+            for (i = 0; i < result.length; i++) {
+                img_id = result[i].img_id;
+                $("#imglist").append('<div style="display:flex;"><h2 id="' + 'img_name' + img_id + '" style="height:30px;width:75%;margin:0;margin-left:20px;margin-top: 20px;">' + result[i].img_name + '</h2>'
+                    + '<h2 id="' + 'sysdate' + img_id + '" style="height:30px;width:20%;margin:0;text-align:right;margin-top: 20px;">' + result[i].sysdate + '</h2></div>');
+                $("#imglist").append('<div style="display:flex;"><h3 id="' + 'account' + img_id + '" style="margin:0;margin-left:20px;">' + 'by ' + result[i].account + '</h3>'
+                    + '<p id="' + 'follow' + img_id + '" class="follow" style="font-size:12px;margin:0;margin-left:50px;cursor:pointer;border-radius:50px;background-color:#FFF;border:1px solid #1da1f2;color:#1da1f2;width:90px; text-align:center">FOLLOW ME</p>'
+                    + '<p style="font-size:12px;margin:0;margin-left:50px">いいね</p><img id="' + 'good' + img_id + '"src="img/heart_blank.png" class="good" style="height:15px;width:15px;margin:0;margin-left:10px;cursor:pointer;"></div>');
+                $("#imglist").append('<div id="' + 'img' + img_id + '" style="display:flex; height:220px;margin-left:15px;"></div>');
+                $("#" + 'img' + img_id).append('<div class="del" style="display:none;width:5%;"><p id="del_' + result[i].img_id + '" class="del_btn" style="cursor: pointer;font-size:30px;text-align:center;">☒</p></div>');
+                $("#" + 'img' + img_id).append('<div style="width:15%;"><img id=imgdata"' + img_id + '" src="' + result[i].img_data + '" style="margin:10px 10px;object-fit:contain;max-width:90%;max-height:200px;"></div>');
+                $("#" + 'img' + img_id).append('<text id="' + 'abstract' + img_id + '" style="height:195px; margin:10px 10px; width:45%;border-style:solid;border-width:0.5px;padding:5px;overflow: auto;" readonly>' + result[i].abstract + '</text>');
+                $("#" + 'img' + img_id).append('<div id=msg' + img_id + '" style="height:175px; margin:10px 10px; width:35%;">' + '</div>');
+                $("#" + 'img' + img_id).append('<div style="display:none"><p id="user' + img_id + '">' + result[i].user_id + '</p></div>');
+            };
+        }
+    });
 };
 
 
